9e24e79ba2370709da6316d3c97341a87ce35eb0
Thangnt <thangnt336@gmail.com>
2021-04-07 22:27:03 -0400

    edited readme file and added comments and other functions in js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
+// Function to generate randomized password base on user input.
 function generatePassword() {
   var passwordLength = Number(window.prompt('Please select the length of your password (8 to 128 characters).'));
 
+  // Executes when the user inputs anything other than a number 8 to 128.
   if (passwordLength < 8 || passwordLength > 128) {
     alert('Password must be between 8 and 128 characters. Please try again');
     return null
@@ -13,16 +15,19 @@ function generatePassword() {
     return null
   }
 
+  // This creates the variables for the password characters.
   var uppercase, lowercase, numbercount, specialOption = false;
   var letters = 'abcdefghijklmnopqrstuvwxyz'
   var numeric = '0123456789'
   var specialCharacter = '\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\]\^\_\`\{\|\}\~'
 
+  // These confirmation functions prompts the user what variables they would like in their password.
   uppercase = confirm('Would you like to include uppercase letters?');
   lowercase = confirm('Would you like to include lowercase letters?');
   numbercount = confirm('Would you like to include numbers?');
   specialOption = confirm('Would you like to include special characters (\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\]\^\_\`\{\|\}\~) ?');
 
+  // Concatenates strings of the variables selected by the user into one string. 
   var userPassword = '';
 
   if (uppercase === true) {
@@ -38,11 +43,13 @@ function generatePassword() {
     userPassword += specialCharacter
   }
 
+  // This makes sure the user selects at least one option for their password or else it prompts the user to try again.
   if (uppercase === false && lowercase === false && numbercount === false && specialOption === false) {
     alert('You must select at least one option. Please try again')
     return null
   }
 
+  // This loop randomizes the variables the user selected by the password length and returns a new string. 
   var genPassword = ''
 
   for (var i = 0; i < passwordLength; i++) {
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Password-Generator
+
+
+## Description
+
+This application generates random passwords based on the criteria the user selected. This application runs in the browser features a dynamically updated HTML and CSS powered by JavaScript code. The password generator creates a password 8 to 128 characters long with letters, numbers, and/or special characters. The user will be prompted if the application is not provided with sufficient information to generate a viable password.
+
+
+## Link
+
+https://thangnt336.github.io/Password-Generator/
+
+## Usage
+
+![first screenshot](assets/screen1.png)
+![second screenshot](assets/screen2.png)
+![third screenshot](assets/screen3.png)
+
+
+
